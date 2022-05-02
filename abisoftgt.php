c43cd9f12a9d8d9163da8285f2e3aae29756cd67
ilemusv1 <91931090+ilemusv1@users.noreply.github.com>
2022-05-02 09:49:01 -0600

    Add files via upload

---
+++ abisoftgt.php
@@ -0,0 +1,8 @@
+<?php 
+
+	echo "#HOLA MUNDO";
+
+	function AbisoftInc(){
+		RETURN 0;
+	}
+?>
---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+    $title ="Sobre Nosotros| ";
+    include "head.php";
+    include "sidebar.php";  
+?>
+
+    <div class="right_col" role="main"> <!-- page content -->
+        <div class="">
+            <div class="page-title">
+            <br>
+                <div class="row">
+            	    <div class="col-md-6 col-md-offset-3">
+                        <h1>Acerca del autor</h1>
+                        <p>Grupo #3 del curso de Ingenieria de Software</p>
+
+                        <br><br>
+
+                        <h2>Help desk - <small>Sistema de Tickets de Soporte</small></h2>
+                        <p>Es un sistema de tokens</p>
+
+                        <h1 style="text-align: center;">Help Desk - UMG</h1>
+                        <h3>Descripcion</h3>
+                        <p>Help desk es un sistema para gestionar tickets de soporte usando proyectos, categorias, prioridades y mas, el sistema esta desarrollado con PHP y MySQL.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /page content -->
+
+<?php include "footer.php" ?>
